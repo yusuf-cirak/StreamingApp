2972b75bf129b929491e9ef97f6279678c9e19a9
Yusuf Çırak <yusuf10901@hotmail.com>
2023-12-16 22:49:44 +0300

    Throw exception when SaveChanges fail

--- backend/src/Infrastructure/Persistence/EntityFramework/Contexts/BaseDbContext.cs
+++ backend/src/Infrastructure/Persistence/EntityFramework/Contexts/BaseDbContext.cs
@@ -3,21 +3,31 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using SharedKernel;
 using System.Reflection;
+using Application.Common.Exceptions;
+using MediatR;
 
 namespace Infrastructure.Persistence.EntityFramework.Contexts;
 
 public class BaseDbContext : DbContext
 {
-    private IConfiguration Configuration { get; }
+    private IPublisher Publisher { get; }
+    protected IConfiguration Configuration { get; }
 
     public virtual DbSet<User> Users { get; set; }
     public virtual DbSet<Streamer> Streamers { get; set; }
     public virtual DbSet<RefreshToken> RefreshTokens { get; set; }
 
+    public virtual DbSet<OperationClaim> OperationClaims { get; set; }
 
-    public BaseDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
+    public virtual DbSet<UserRoleClaim> UserRoleClaims { get; set; }
+
+    public virtual DbSet<RoleOperationClaim> RoleOperationClaims { get; set; }
+
+
+    public BaseDbContext(DbContextOptions options, IConfiguration configuration, IPublisher mediator) : base(options)
     {
         Configuration = configuration;
+        Publisher = mediator;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -27,10 +37,9 @@ public class BaseDbContext : DbContext
 
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
+        var auditableEntities = ChangeTracker.Entries<AuditableEntity>();
 
-        var datas = ChangeTracker.Entries<AuditableEntity>();
-
-        foreach (var data in datas)
+        foreach (var data in auditableEntities)
         {
             _ = data.State switch
             {
@@ -40,6 +49,13 @@ public class BaseDbContext : DbContext
             };
         }
 
-        return await base.SaveChangesAsync(cancellationToken);
+        var saveResult = await base.SaveChangesAsync(cancellationToken);
+
+        if (saveResult is 0)
+        {
+            throw new DatabaseOperationFailedException("Could not save changes to database");
+        }
+
+        return saveResult;
     }
 }
