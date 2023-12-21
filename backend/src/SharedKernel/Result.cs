52eba6b1ed3b9d7c8bbf7a9993ac35d4d8b7fa99
Yusuf Çırak <yusuf10901@hotmail.com>
2023-12-21 18:38:37 +0300

    Result pattern with ToHttpResponse implementation

---
+++ backend/src/SharedKernel/Result.cs
@@ -0,0 +1,36 @@
+﻿namespace SharedKernel;
+
+public readonly record struct Result<TValue, TError>
+{
+    private readonly TValue? _value;
+    private readonly TError? _error;
+
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    private Result(TValue value)
+    {
+        _value = value;
+        _error = default;
+        IsSuccess = true;
+    }
+
+    private Result(TError error)
+    {
+        _value = default;
+        _error = error;
+        IsSuccess = false;
+    }
+
+    public static Result<TValue, TError> Ok(TValue value) => new(value);
+
+    public static Result<TValue, TError> Fail(TError error) => new(error);
+
+    public static implicit operator Result<TValue, TError>(TValue value) => Ok(value);
+
+    public static implicit operator Result<TValue, TError>(TError error) => Fail(error);
+
+    public TResult Match<TResult>(Func<TValue, TResult> success, Func<TError, TResult> failure)
+        => IsSuccess ? success(_value!) : failure(_error!);
+}
---
+++ backend/src/SharedKernel/ResultExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SharedKernel;
+
+public static class ResultExtensions
+{
+    public static IResult ToHttpResponse<TValue, TError>(this Result<TValue, TError> result)
+        where TError : ProblemDetails
+    {
+        return result.Match(
+            Results.Ok,
+            failure => failure.Status switch
+            {
+                400 => Results.BadRequest(failure),
+                401 => Results.Unauthorized(),
+                403 => Results.Forbid(),
+                404 => Results.NotFound(failure),
+                409 => Results.Conflict(failure),
+                500 => Results.Problem(failure),
+                _ => Results.BadRequest(failure)
+            });
+    }
+}
