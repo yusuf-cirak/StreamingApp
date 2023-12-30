19a268d954e0ecce889ee68fedc652e90895ef9a
Yusuf Çırak <yusuf10901@hotmail.com>
2023-12-30 16:08:20 +0300

    Common authorization rules

---
+++ backend/src/Application/Common/Rules/CommonAuthorizationRules.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using System.Text.Json;
+using Application.Common.Errors;
+using Application.Features.Roles.Dtos;
+
+namespace Application.Common.Rules;
+
+public static class CommonAuthorizationRules
+{
+    public static Result UserMustBeAdmin(ICollection<Claim> claims, object request)
+    {
+        var rolesString = claims.FirstOrDefault(claim => claim.Type == "Roles")?.Value ?? String.Empty;
+
+        if (string.IsNullOrEmpty(rolesString))
+        {
+            return Result.Failure(RoleErrors.DoesNotExist);
+        }
+
+        var roles = JsonSerializer.Deserialize<List<GetUserRoleDto>>(rolesString);
+
+        if (!roles.Exists(r => r.Role.Name == RoleConstants.SystemAdmin))
+        {
+            return Result.Failure(AuthorizationErrors.Unauthorized());
+        }
+
+
+        return Result.Success();
+    }
+}
