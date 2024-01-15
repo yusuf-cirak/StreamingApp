f6630c56b5747747551cf4f7281cf663c96eb751
Yusuf Çırak <yusuf10901@hotmail.com>
2024-01-15 23:40:03 +0300

    creator layout fix

--- frontend/src/app/core/layouts/creator/creator.layout.ts
+++ frontend/src/app/core/layouts/creator/creator.layout.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { NgIf, NgStyle } from '@angular/common';
 import { CreatorNavbarComponent } from './components/navbar/creator-navbar.component';
 import { CreatorSidebarComponent } from './components/sidebar/creator-sidebar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-creator',
   standalone: true,
-  imports: [CreatorNavbarComponent, CreatorSidebarComponent, NgStyle, NgIf],
+  imports: [
+    CreatorNavbarComponent,
+    CreatorSidebarComponent,
+    NgStyle,
+    NgIf,
+    RouterOutlet,
+  ],
   templateUrl: './creator.layout.html',
 })
 export class CreatorLayout {}
