f5f9ecfa23df8e0999917e5b713fa11db3c61d22
Yusuf Çırak <yusuf10901@hotmail.com>
2024-03-11 00:28:34 +0300

    refactor(frontend):loading to placeholder

--- frontend/src/app/core/modules/streams/stream.component.ts
+++ frontend/src/app/core/modules/streams/stream.component.ts
@@ -2,10 +2,7 @@ import { OfflineStreamComponent } from './components/offline-stream/offline-stre
 import { ActivatedRoute } from '@angular/router';
 import { Component, computed, inject } from '@angular/core';
 import { NgTemplateOutlet } from '@angular/common';
-import { map } from 'rxjs';
-import { toSignal } from '@angular/core/rxjs-interop';
 import { LiveStreamComponent } from './components/live-stream/live-stream.component';
-import { StreamState } from './models/stream-state';
 import { StreamFacade } from './services/stream.facade';
 import { NotFoundStreamComponent } from './components/not-found-stream/not-found-stream.component';
 
@@ -13,20 +10,19 @@ import { NotFoundStreamComponent } from './components/not-found-stream/not-found
   selector: 'app-stream',
   standalone: true,
   templateUrl: './stream.component.html',
-  imports: [NgTemplateOutlet, LiveStreamComponent, OfflineStreamComponent,NotFoundStreamComponent],
+  imports: [
+    NgTemplateOutlet,
+    LiveStreamComponent,
+    OfflineStreamComponent,
+    NotFoundStreamComponent,
+  ],
 })
 export class StreamComponent {
   readonly route = inject(ActivatedRoute);
 
   readonly streamFacade = inject(StreamFacade);
 
-  streamState = computed(()=>this.streamFacade.streamState())
+  streamState = computed(() => this.streamFacade.streamState());
 
-  liveStream = computed(()=>this.streamFacade.liveStream())
-
-  ngOnInit(){
-    console.log(this.streamState())
-  }
-
-
+  liveStream = computed(() => this.streamFacade.liveStream());
 }
