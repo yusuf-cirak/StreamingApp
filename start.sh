603cfed20bde578c1b813990afc24f4cf83a06d7
Yusuf Çırak <yusuf10901@hotmail.com>
2024-06-09 22:37:08 +0300

    chore: Update environment variables in deploy.yaml and docker-compose.yml

--- start.sh
+++ start.sh
@@ -3,4 +3,7 @@ export STREAM_API_KEY="${STREAM_API_KEY:-AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY
 
 export ASPNETCORE_ENVIRONMENT="${ASPNETCORE_ENVIRONMENT:-Docker}"
 
+export POSTGRES_USER="${POSTGRES_USER:-postgres}"
+export POSTGRES_PASSWORD="${POSTGRES_PASSWORD:-postgres}"
+
 docker compose up --force-recreate --build -d && docker container restart nginx && docker ps
