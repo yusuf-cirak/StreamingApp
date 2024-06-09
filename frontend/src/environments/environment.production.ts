a9f59745c7aaed1abacf06c42273e0dffe886784
Yusuf Çırak <yusuf10901@hotmail.com>
2024-06-09 18:54:41 +0300

    feat: Deployment with github actions, updating environment variables in frontend, adding start.sh and updating docker compose

---
+++ frontend/src/environments/environment.production.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  apiUrl: 'https://stream.yusufcirak.net/api',
+  hlsUrl: 'https://stream.yusufcirak.net/hls',
+  streamHubUrl: 'https://stream.yusufcirak.net/_stream',
+  defaultProfileImageApiUrl: 'https://ui-avatars.com/api',
+  cloudinary: {
+    publicKey: 'dhcu4h56y',
+    baseUrl:
+      'https://res.cloudinary.com/dhcu4h56y/image/upload/f_auto,q_auto/v1',
+    folderNames: {
+      streamThumbnails: 'stream_thumbnails',
+      profileImages: 'profile_images',
+    },
+  },
+};
---
+++ start.sh
@@ -0,0 +1,6 @@
+export STREAM_API_BASE_URL="http://backend:8080/api"
+export STREAM_API_KEY = "${STREAM_API_KEY:-AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY}"
+
+export ASPNETCORE_ENVIRONMENT="${ASPNETCORE_ENVIRONMENT:-Docker}"
+
+docker compose up --force-recreate --build -d && docker container restart nginx && docker ps
